8ed3712c2ac02313760b2cc45f7766413d52de1c
Your name <you@example.com>
2021-07-19 04:33:14 -0500

    commit tree cd8402ddd2eb1f8b411c44982e0c53d6b044f556

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "LUKKAAAA is the champion";
+
+
+
